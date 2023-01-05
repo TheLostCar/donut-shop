de0b1d24e9b4ad8aebfe60bda07bbeaa736fdcc6
Carlos Aranzana <carlos.aranzana2001@gmail.com>
2023-01-05 00:20:55 -0500

    add Image to DelightToAllSenses

--- components/DelightToAllSenses/DelightToAllSenses.tsx
+++ components/DelightToAllSenses/DelightToAllSenses.tsx
@@ -1,10 +1,11 @@
+import Image from 'next/image';
 import styles from './DelightToAllSenses.module.scss';
 
 const DelightToAllSenses = () => {
     return (
         <div className={styles.container}>
             <div className={styles.text}>
-                <h2>Delicious to the eyes and mouth</h2>
+                <h2>A treat to all 5 senses</h2>
 
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Error similique eveniet dignissimos molestias, consequatur non pariatur quisquam at enim dolorum aut rem natus, in maiores quasi provident ea. Eligendi, reiciendis?
@@ -12,7 +13,11 @@ const DelightToAllSenses = () => {
             </div>
 
             <div className={styles.image}>
-
+                <Image
+                    src='https://res.cloudinary.com/dgnrqyibk/image/upload/donut-shop/donut_assets/donutThreeStack_udttne.png'
+                    alt={''}
+                    fill
+                />
             </div>
         </div>
     );
